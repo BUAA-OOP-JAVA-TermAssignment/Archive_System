0aa66b503039a5a768391a42b234088d9fda18ad
AkashiSensei <fengzhiyuyi2013@gmail.com>
2022-12-05 00:13:11 +0800

    开始写图形界面

--- src/view/MyFrame.java
+++ src/view/MyFrame.java
@@ -5,11 +5,13 @@ import javax.swing.*;
 abstract public class MyFrame extends JFrame {
     public MyFrame() {
         this.setSize(1280, 720);
-        this.setLayout(null);
-
         this.setTitle("北航文献管理系统");
         this.setLocationRelativeTo(null);
 
+        this.setLayout(null);
 
+        //TODO:设置一个可爱的图标
+
+        //TODO:添加个人中心头像及进入按钮
     }
 }
