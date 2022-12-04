9c8bb65e15f7c98777e41bc9a908ab60c1c4cf3f
AkashiSensei <fengzhiyuyi2013@gmail.com>
2022-12-04 23:26:23 +0800

    开始写图形界面

---
+++ src/view/MyFrame.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+abstract public class MyFrame extends JFrame {
+    public MyFrame() {
+        this.setSize(1280, 720);
+        this.setLayout(null);
+
+        this.setTitle("北航文献管理系统");
+        this.setLocationRelativeTo(null);
+
+
+    }
+}
