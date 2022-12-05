454169d1d3891e8999504719a6bedebb5977a1a4
pcpas <1134380859@qq.com>
2022-12-05 18:53:05 +0800

    client HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld but current time is 23:27 4/12");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 8888);
+        OutputStream os = socket.getOutputStream();
+        os.write("Hello?".getBytes());
+        InputStream is = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int len = is.read(bytes);
+        System.out.println(new String(bytes, 0, len));
+        socket.close();
     }
 }
