07f2ca6ddc844055212c4334cb38e7664abfb471
Hathoric <WYK827906540@163.com>
2022-12-08 00:02:03 +0800

    还没有实现发送报文

--- src/message/UserRegisterMsg.java
+++ src/message/UserRegisterMsg.java
@@ -1,4 +1,24 @@
 package message;
 
+import java.util.Date;
+
 public class UserRegisterMsg {
+    private int id;
+    private String userName;
+    private String password;
+    private String email;
+    private Date createTime;
+
+    public UserRegisterMsg(int id, String userName, String password, String email, Date createTime){
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.createTime = createTime;
+    }
+    private int getId(){return id;}
+    public String getUserName(){return userName;}
+    public String getPassword(){return password;}
+    public String getEmail(){return email;}
+    public Date getCreateTime(){return createTime;}
 }
