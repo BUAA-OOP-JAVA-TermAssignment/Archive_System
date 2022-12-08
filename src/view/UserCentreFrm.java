275a5801c11e89f5ce2ac0ee9041466d74be97a0
Liangff <3232572736@qq.com>
2022-12-08 22:48:33 +0800

    个人界面完成

--- src/view/UserCentreFrm.java
+++ src/view/UserCentreFrm.java
@@ -53,6 +53,24 @@ public class UserCentreFrm extends MyInterFrame{
 
         Container container = this.getContentPane();
 
+        JLabel jWrongPasswordType = new JLabel();
+        jWrongPasswordType.setText("密码不合法");
+        jWrongPasswordType.setForeground(Color.RED);
+        jWrongPasswordType.setBounds(WIDGET_X,20+4*FIELD_HEIGHT,WIDGET_GAP,50);
+        jWrongPasswordType.setHorizontalAlignment(JLabel.RIGHT);
+        jWrongPasswordType.setVerticalAlignment(JLabel.CENTER);
+        jWrongPasswordType.setVisible(false);
+        container.add(jWrongPasswordType);
+
+        JLabel jWrongRePassword = new JLabel();
+        jWrongRePassword.setText("密码不一致");
+        jWrongRePassword.setForeground(Color.RED);
+        jWrongRePassword.setBounds(WIDGET_X,20+5*FIELD_HEIGHT,WIDGET_GAP,50);
+        jWrongRePassword.setHorizontalAlignment(JLabel.RIGHT);
+        jWrongRePassword.setVerticalAlignment(JLabel.CENTER);
+        jWrongRePassword.setVisible(false);
+        container.add(jWrongRePassword);
+
         JLabel jLabelUserName = new JLabel();
         jLabelUserName.setText(this.userName);
         jLabelUserName.setBounds(WIDGET_X,20,WIDGET_GAP,50);
@@ -71,27 +89,34 @@ public class UserCentreFrm extends MyInterFrame{
         jLabelEmail.setFont(MyFonts.SUB_TITLE_FONT_24);
         container.add(jLabelEmail);
 
+        JLabel jLabelTip = new JLabel();
+        jLabelTip.setText("<html>注意：密码应该为8-16位，只能由大小写字母，下划线和数字中的一种或几种组成，要求以字母开头</html>");
+        jLabelTip.setBounds(WIDGET_X,20+3*FIELD_HEIGHT,WIDGET_GAP,50);
+        //jLabelTip.setFont(MyFonts.TEXT_FONT_18);
+        jLabelTip.setVisible(false);
+        container.add(jLabelTip);
+
         JLabel jLabelPassword = new JLabel();
         jLabelPassword.setText("新密码：");
-        jLabelPassword.setBounds(WIDGET_X,20+3*FIELD_HEIGHT,WIDGET_GAP/2,50);
+        jLabelPassword.setBounds(WIDGET_X,20+4*FIELD_HEIGHT,WIDGET_GAP/2,50);
         jLabelPassword.setFont(MyFonts.SUB_TITLE_FONT_24);
         jLabelPassword.setVisible(false);
         container.add(jLabelPassword);
 
         JLabel jLabelRePassword = new JLabel();
         jLabelRePassword.setText("确认密码：");
-        jLabelRePassword.setBounds(WIDGET_X,20+4*FIELD_HEIGHT,WIDGET_GAP,50);
+        jLabelRePassword.setBounds(WIDGET_X,20+5*FIELD_HEIGHT,WIDGET_GAP,50);
         jLabelRePassword.setFont(MyFonts.SUB_TITLE_FONT_24);
         jLabelRePassword.setVisible(false);
         container.add(jLabelRePassword);
 
         JPasswordField jPasswordFieldNew = new JPasswordField();
-        jPasswordFieldNew.setBounds(WIDGET_X+WIDGET_GAP/2,30+3*FIELD_HEIGHT,WIDGET_GAP/2,30);
+        jPasswordFieldNew.setBounds(140,30+4*FIELD_HEIGHT,WIDGET_GAP/2,30);
         jPasswordFieldNew.setVisible(false);
         container.add(jPasswordFieldNew);
 
         JPasswordField jPasswordFieldRenew = new JPasswordField();
-        jPasswordFieldRenew.setBounds(WIDGET_X+WIDGET_GAP/2,30+4*FIELD_HEIGHT,WIDGET_GAP/2,30);
+        jPasswordFieldRenew.setBounds(140,30+5*FIELD_HEIGHT,WIDGET_GAP/2,30);
         jPasswordFieldRenew.setVisible(false);
         container.add(jPasswordFieldRenew);
 
@@ -101,14 +126,22 @@ public class UserCentreFrm extends MyInterFrame{
         passwordWin.setResizable(false);
         passwordWin.setSize(300,200);
 
+
+        JLabel jErrorWrongPassword = new JLabel();
+        jErrorWrongPassword.setText("密码输入错误");
+        jErrorWrongPassword.setForeground(Color.RED);
+        jErrorWrongPassword.setBounds(WIDGET_X,10,300-3*WIDGET_X,30);
+        jErrorWrongPassword.setHorizontalAlignment(JLabel.RIGHT);
+        jErrorWrongPassword.setVisible(false);
+        passwordWin.add(jErrorWrongPassword);
+
         JLabel jLabelInput = new JLabel();
         jLabelInput.setText("请输入初始密码：");
-        jLabelInput.setBounds(20,10,WIDGET_GAP,30);
-        jLabelInput.setFont(MyFonts.TEXT_FONT_18);
+        jLabelInput.setBounds(WIDGET_X,10,WIDGET_GAP,30);
         passwordWin.add(jLabelInput);
 
         JPasswordField jPasswordField = new JPasswordField();
-        jPasswordField.setBounds(2*WIDGET_X,FIELD_HEIGHT,WIDGET_GAP/2,30);
+        jPasswordField.setBounds(WIDGET_X,FIELD_HEIGHT,240,30);
 
         JButton jButtonChangePassword = new JButton();
         jButtonChangePassword.setText("修改密码");
@@ -120,10 +153,11 @@ public class UserCentreFrm extends MyInterFrame{
             passwordWin.setVisible(true);
         });
 
+        // 确认使用新密码的函数
         JButton jButtonCancelPassword = new JButton();
         JButton jButtonContinueChange = new JButton();
         jButtonContinueChange.setText("确认修改");
-        jButtonContinueChange.setBounds(20,40+5*FIELD_HEIGHT,WIDGET_GAP/2-20,30);
+        jButtonContinueChange.setBounds(20,40+6*FIELD_HEIGHT,WIDGET_GAP/2-20,30);
         jButtonContinueChange.setVisible(false);
         jButtonContinueChange.addActionListener(evt ->{
             System.out.println("UserCentre : Click continue change password button");
@@ -140,20 +174,26 @@ public class UserCentreFrm extends MyInterFrame{
                     jPasswordFieldRenew.setVisible(false);
                     jPasswordFieldNew.setText("");
                     jPasswordFieldRenew.setText("");
+                    jWrongPasswordType.setVisible(false);
+                    jWrongRePassword.setVisible(false);
+                    jLabelTip.setVisible(false);
                 } else {
-                    //TODO:ERROR1
+                    jWrongRePassword.setVisible(true);
+                    jPasswordFieldRenew.setText("");
                 }
             } else {
-                //TODO:ERROR2
+                jWrongPasswordType.setVisible(true);
+                jPasswordFieldNew.setText("");
+                jPasswordFieldRenew.setText("");
             }
 
         });
         container.add(jButtonContinueChange);
 
-
+        // 关闭修改密码界面的函数
         jButtonCancelPassword.setText("取消修改");
         jButtonCancelPassword.setVisible(false);
-        jButtonCancelPassword.setBounds(WIDGET_GAP/2+20,40+5*FIELD_HEIGHT,WIDGET_GAP/2-20,30);
+        jButtonCancelPassword.setBounds(WIDGET_GAP/2+20,40+6*FIELD_HEIGHT,WIDGET_GAP/2-20,30);
         jButtonCancelPassword.addActionListener(evt ->{
             System.out.println("UserCentre : Click cancel change password button");
             jButtonChangePassword.setVisible(true);
@@ -163,11 +203,17 @@ public class UserCentreFrm extends MyInterFrame{
             jLabelRePassword.setVisible(false);
             jPasswordFieldNew.setVisible(false);
             jPasswordFieldRenew.setVisible(false);
+            jWrongPasswordType.setVisible(false);
+            jWrongRePassword.setVisible(false);
+            jLabelTip.setVisible(false);
             jPasswordFieldNew.setText("");
             jPasswordFieldRenew.setText("");
         });
         container.add(jButtonCancelPassword);
 
+
+        // 输入旧密码确认的函数
+        //TODO：需要加回车逻辑
         JButton jButtonYes = new JButton();
         jButtonYes.setText("确认");
         jButtonYes.setBounds(30,80,100,30);
@@ -175,6 +221,7 @@ public class UserCentreFrm extends MyInterFrame{
             System.out.println("UserCentreFrmWin : Click yes button");
             oldPassword = new String(jPasswordField.getPassword());
             if(oldPassword.matches(this.password)){
+                jErrorWrongPassword.setVisible(false);
                 jButtonChangePassword.setVisible(false);
                 jLabelPassword.setVisible(true);
                 jLabelRePassword.setVisible(true);
@@ -182,11 +229,13 @@ public class UserCentreFrm extends MyInterFrame{
                 jButtonCancelPassword.setVisible(true);
                 jPasswordFieldNew.setVisible(true);
                 jPasswordFieldRenew.setVisible(true);
+                jLabelTip.setVisible(true);
                 jPasswordField.setText("");
                 passwordWin.setVisible(false);
                 passwordWin.dispose();
             } else {
-                //TODO :加一个错误提示
+                jErrorWrongPassword.setVisible(true);
+                jPasswordField.setText("");
             }
         });
 
@@ -195,12 +244,12 @@ public class UserCentreFrm extends MyInterFrame{
         jButtonNo.setBounds(150,80,100,30);
         jButtonNo.addActionListener(evt -> {
             System.out.println("UserCentreFrmWin : Click no button");
-
+            jPasswordField.setText("");
+            jErrorWrongPassword.setVisible(false);
         });
 
 
         passwordWin.add(jPasswordField);
-        //passwordWin.add(jButtonNo);
         passwordWin.add(jButtonYes);
         passwordWin.add(jButtonNo);
         passwordWin.setVisible(false);
