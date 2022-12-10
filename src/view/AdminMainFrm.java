7ceb8ac1d8703aaf515f66a7e7fe3692ec270e5d
AkashiSensei <fengzhiyuyi2013@gmail.com>
2022-12-10 23:19:49 +0800

    修正了管理员窗口的关闭逻辑

--- src/view/AdminMainFrm.java
+++ src/view/AdminMainFrm.java
@@ -1,5 +1,16 @@
 package view;
 
+import controller.Closer;
+
 public class AdminMainFrm extends MyFrame {
+    private static final AdminMainFrm adminMainFrm = new AdminMainFrm();
+
+    private AdminMainFrm() {
+        this.addWindowListener(Closer.getNoDataUploadCloser());
+    }
+
+    public static AdminMainFrm getInstance() {
+        return adminMainFrm;
+    }
 
 }
