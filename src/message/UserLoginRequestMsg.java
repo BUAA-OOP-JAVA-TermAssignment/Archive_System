efe23f1916c5b9321058c55251e0f8a99ad53f1b
pcpas <1134380859@qq.com>
2022-12-11 18:25:33 +0800

    修改部分msg，使其支持序列化

--- src/message/UserLoginRequestMsg.java
+++ src/message/UserLoginRequestMsg.java
@@ -6,12 +6,13 @@ package message;
  * @author : AkashiSensei
  * &#064;date  : 2022/12/8 20:19
  */
-public class UserLoginRequestMsg extends BaseMsg{
+public class UserLoginRequestMsg extends BaseMsg {
     public static final int GUEST = 1, ADMIN = 2;
     private int userType;
     private String id;
     private String password;
-    private UserLoginRequestMsg(int userType,String id, String password) {
+
+    private UserLoginRequestMsg(int userType, String id, String password) {
         super(LOGIN);
         this.userType = userType;
         this.id = id;
@@ -20,7 +21,8 @@ public class UserLoginRequestMsg extends BaseMsg{
 
     /**
      * 创建LoginRequestMsg实例
-     * @param id 用户学工号
+     *
+     * @param id       用户学工号
      * @param password 用户密码
      * @return 返回新建的实例
      */
@@ -28,7 +30,9 @@ public class UserLoginRequestMsg extends BaseMsg{
         return new UserLoginRequestMsg(userType, id, password);
     }
 
-    public int getUserType(){return userType;}
+    public int getUserType() {
+        return userType;
+    }
 
     public String getId() {
         return id;
